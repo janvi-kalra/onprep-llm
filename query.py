454c50dc7a3bdfbd5230f9a3ac481c7d7425e7fe
Stephen Zerfas <stephen.zerfas@gmail.com>
2023-06-10 16:42:47 -0700

    add test_query

--- query.py
+++ query.py
@@ -1,44 +1,70 @@
+from typing import List
 
-from .embed import embed_list
+from chroma import getRelevantResponses
+from embed import embed_list
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 ####################################
 ## HELPERS 
 ####################################
 
 # TODO 
-def retrieve_from_chroma(embedded_query:list) -> dict:
-    """Retrieve the embedded text from Chroma. 
-    """
-    return False 
+# def retrieve_from_chroma(embedded_query:list) -> dict:
+#     """Retrieve the embedded text from Chroma.
+#     """
+#     return False
 
-def get_retrievals(query:str) -> dict:
-    embedded_query = embed_list(query)
-    relevant_retrievals = retrieve_from_chroma(embedded_query)
-    return relevant_retrievals
-
-# TODO 
-def format_prompt(query:str, relevant_retrievals:dict) -> str:
-    """Format the prompt for the user. 
-    """
-    return False
+# def get_retrievals(query:str) -> dict:
+#     embedded_query = embed_list(query)
+#     relevant_retrievals = retrieve_from_chroma(embedded_query)
+#     return relevant_retrievals
 
 # TODO 
-def get_completion_from_openassistant(prompt:str) -> str:
+# def format_prompt(query:str, relevant_retrievals:dict) -> str:
+#     """Format the prompt for the user.
+#     """
+#     return False
+
+
+def format_query_system_context(relevant_retrievals: List[str]) -> str:
+    """Format system-level prompt for the user"""
+    context = ' '.join([r[0] for r in relevant_retrievals])
+    return f'You are an expert IFS Therapist, and you are helping someone process their memories. ' \
+           f'Here are some relevant memories from the person’s journal. ' \
+           f'Please use this context to help them answer their questions: ' \
+           f'{context} ' \
+
+# TODO 
+def get_completion_from_openassistant(query: str, relevant_retrievals: List[str]) -> str:
     """Get the completion from OpenAssistant. 
     """
-    return False
+    syste_context = format_query_system_context(relevant_retrievals)
+    import pdb; pdb.set_trace()
+    resp = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": syste_context},
+            {"role": "user", "content": query},
+        ]
+    )
+    completion = resp['choices'][0]['message']['content']
+    return completion
+
 
 ####################################
 ## MAIN 
 ####################################
-
 def run_query(query:str) -> dict: 
     """Run a query against the Chroma API 
     """
-    query_embed = embed_list([query])
-    relevant_retrievals = retrieve_from_chroma(query_embed)
-    prompt = format_prompt(query, relevant_retrievals)
-    completion = get_completion_from_openassistant(prompt)
+    # query_embed = embed_list([query])
+    relevant_retrievals = getRelevantResponses(query)
+    import pdb; pdb.set_trace()
+    completion = get_completion_from_openassistant(query, relevant_retrievals)
     return completion
 
 
---
+++ test_query.py
@@ -0,0 +1,43 @@
+# import requests
+#
+# API_URL = "https://api-inference.huggingface.co/models/OpenAssistant/falcon-7b-sft-top1-696"
+# API_TOKEN = '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'
+# headers = {"Authorization": f"Bearer {API_TOKEN}"}
+#
+# def query(payload):
+#     response = requests.post(API_URL, headers=headers, json=payload)
+#     return response.json()
+#
+# output = query({
+#     "inputs": "Can you please let us know more details about your ",
+# })
+
+# import openai
+# OPENAI_API_KEY = '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'
+# openai.api_key = OPENAI_API_KEY
+#
+# resp = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Who won the world series in 2020?"},
+#         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+#         {"role": "user", "content": "Where was it played?"}
+#     ]
+# )
+import chroma
+import googledocs
+from chroma import getRelevantResponses
+from query import run_query, get_completion_from_openassistant
+
+google_link = 'https://docs.google.com/document/d/1ukOID1wlstzmE1kC8CMZkiz-zDrgOtCNtgcdHXg7vZg/edit'
+input_data = googledocs.getContent(google_link)
+# Process the input_data with your Python code here
+chroma.addToCollection(input_data, 'none')
+# run_query('LLM')
+query = 'LLM'
+resp = getRelevantResponses(query)
+completion = get_completion_from_openassistant(query, resp['documents'])
+import pdb; pdb.set_trace()
+
+
