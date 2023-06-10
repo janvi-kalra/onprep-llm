4802e58266a2afdd9c17076876df1b8616f51214
Stephen Zerfas <stephen.zerfas@gmail.com>
2023-06-10 12:44:06 -0700

    add second query endpoint

--- views.py
+++ views.py
@@ -14,3 +14,12 @@ def submit():
         # Process the input_data with your Python code here
         processed_data = embed.test(input_data)  # Replace with your own processing logic
         return render_template("result.html", processed_data=processed_data)
+
+
+@views.route("/query", methods=["POST"])
+def query():
+    if request.method == "POST":
+        input_data = request.form.get("inputField")
+        # todo:query chroma embeddings database with text to get stored journal entries with related embeddings
+        # todo: query Open Assistant with original query and related journal entries
+        return render_template("result.html", processed_data="Congrats, this will soon work for you!")
