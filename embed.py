6c2339ef7414758eed962ea85064e79c4da38fa8
stephenkfrey <2404105+stephenkfrey@users.noreply.github.com>
2023-06-10 11:49:51 -0700

    Test commit

--- embed.py
+++ embed.py
@@ -6,4 +6,4 @@ sentences = ["This is an example sentence", "Each sentence is converted"]
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 embeddings = model.encode(sentences)
-print(embeddings)
+print(embeddings) 
