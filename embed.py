9374afb608b8ece00b93d63dbb7eaecda3068204
Janvi Kalra <janvi@coda.io>
2023-06-10 12:15:06 -0700

    end to end UI of text turned to embedding works"

--- embed.py
+++ embed.py
@@ -2,8 +2,9 @@
 # responsible for turning text -> embeddings
 
 from sentence_transformers import SentenceTransformer
-sentences = ["This is an example sentence", "Each sentence is converted"]
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-embeddings = model.encode(sentences)
-print(embeddings) 
+def test(sentences):
+    # sentences = ["This is an example sentence", "Each sentence is converted"]
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    embeddings = model.encode(sentences)
+    return embeddings
--- views.py
+++ views.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import embed
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
-def home(): 
+def index(): 
     return render_template("home.html")
+
+@views.route("/submit", methods=["POST"])
+def submit(): 
+    print('IN SUBMIT')
+    if request.method == "POST":
+        input_data = request.form.get("inputField")
+        # Process the input_data with your Python code here
+        processed_data = embed.test(input_data)  # Replace with your own processing logic
+        return render_template("result.html", processed_data=processed_data)
