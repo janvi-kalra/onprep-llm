2fa1a63ef34bb18d835c0724007ad2d69a9e04ce
Janvi Kalra <janvi@coda.io>
2023-06-10 10:41:11 -0700

    embedding model

---
+++ embed.py
@@ -0,0 +1,9 @@
+# https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+# responsible for turning text -> embeddings
+
+from sentence_transformers import SentenceTransformer
+sentences = ["This is an example sentence", "Each sentence is converted"]
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+embeddings = model.encode(sentences)
+print(embeddings)
